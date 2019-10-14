da65f47850944ea234fda0ca390bacb9dac39336
omirajkar <onkar.m@indictranstech.com>
2019-10-14 16:14:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+#from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in frappe/__init__.py
